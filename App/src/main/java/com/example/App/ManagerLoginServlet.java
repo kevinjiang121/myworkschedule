d57920b747a7e29da18401243fc0c1d1e21e1940
Kevin Jiang <kevinjiang121@gmail.com>
2021-04-25 20:48:23 -0400

    Added functionality to ManagerLoginServlet.java

--- App/src/main/java/com/example/App/ManagerLoginServlet.java
+++ App/src/main/java/com/example/App/ManagerLoginServlet.java
@@ -1,27 +1,48 @@
 package com.example.App;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "com.example.App.ManagerLoginServlet", value = "/com.example.App.ManagerLoginServlet")
 public class ManagerLoginServlet extends HttpServlet {
-    private String message;
+    private static final long serialVersionUID = 1L;
+    private EmployeeDao employeeDao = new EmployeeDao();
 
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
         response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/managerLogin.jsp");
+        dispatcher.forward(request, response);
     }
 
-    public void destroy() {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String email = request.getParameter("email");
+        String password = request.getParameter("passd");
+        Employee employee = null;
+
+        try {
+            employee = employeeDao.managerLoggingIn(email, password);
+        } catch(ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if(employee != null && employee.getEmployeeID() > 0 && !employee.getIsManager()) {
+            session.setAttribute("uid", employee.getEmployeeID());
+            session.setAttribute("name", employee.getName());
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/loggedInIndex.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/managerLogin.jsp");
+            dispatcher.forward(request, response);
+        }
     }
 }
